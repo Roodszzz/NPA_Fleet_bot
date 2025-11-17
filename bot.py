80640d1160ea845fd19e58d90ffd371eef196754
Roodszzz <nprudnov@gmail.com>
2025-11-17 10:00:17 +0200

    Monthly form

--- bot.py
+++ bot.py
@@ -1708,55 +1708,13 @@ async def contact_location_callback(update: Update, context: ContextTypes.DEFAUL
     await query.message.reply_text(text=text, reply_markup=reply_markup)
 
 
-#Контакты по локациям
-LOCATIONS = {
-    "shyroke": {
-        "manager": {
-            "name": "Oleksandr Rudnov | Олександр Руднов",
-            "phone": "+380987938674",
-            "email": "OleRud441@npaid.org"
-        },
-        "senior_officer": {
-            "position": "Senior Fleet Officer",
-            "name": "Roman Kucherevskyi",
-            "phone": "+380661930132",
-            "email": "RomKuc884@npaid.org"
-        },
-        "car_washes": [
-            {"name": "Avtoynhulstroy", "phone": "+380 67 633 1025", "map": "https://www.google.com/maps?cid=3778105884522161440"},
-            {"name": "Nova Liniya", "phone": "+380 97 577 2770", "map": "https://www.google.com/maps?cid=1167848751790635382"},
-        ],
-        "tire_services": [
-            {"name": "Avtoynhulstroy", "phone": "+380 67 633 1025", "map": "https://www.google.com/maps?cid=3778105884522161440"},
-            {"name": "Nova Liniya", "phone": "+380 97 577 2770", "map": "https://www.google.com/maps?cid=1167848751790635382"},
-            {"name": "SHYROKE - Tyre service", "phone": "+380 98 455 8113", "map": "https://maps.app.goo.gl/otgcPE4GaHowdxEj8"},
-        ],
-    },
-    "mykolaiv": {
-        "manager": {
-            "name": "Andriy Padalka | Андрій Падалка",
-            "phone": "+380506008345",
-            "email": "AndPad212@npaid.org"
-        },
-        "senior_officer": {
-            "position": "Senior Fleet Officer",
-            "name": "Roman Kucherevskyi",
-            "phone": "+380661930132",
-            "email": "RomKuc884@npaid.org"
-        },
-        "car_washes": [
-            {"name": "AVTOSMILE", "phone": "+380 95 792 35 94", "map": "https://maps.app.goo.gl/Xc2b4q8CWEHERMtL9"},
-            {"name": "VIANOR", "phone": "+380 50 495 60 85", "map": "https://maps.app.goo.gl/HQw66xsakjPp245T6"},
-            {"name": "FOP MARGOLIN", "phone": "+380 63 035 23 02", "map": "https://maps.app.goo.gl/xFFs9e6gsxMEAXccA"},
-            {"name": "AVTORELAX", "phone": "No number, yet", "map": "https://maps.app.goo.gl/YqjpXX8FSgbz3PVV6"},
-            {"name": "FOP Shevchenko", "phone": "+380 97 438 25 38", "map": "https://maps.app.goo.gl/3SU1snMcijN48ehq8"},
-        ],
-        "tire_services": [
-            {"name": "Tire Service 1", "phone": "+380 432 111 001", "map": "https://goo.gl/maps/tire1_mykolaiv"},
-            {"name": "Tire Service 2", "phone": "+380 432 111 002", "map": "https://goo.gl/maps/tire2_mykolaiv"},
-        ],
-    }
-}
+from locations import LOCATIONS
+
+# пример использования
+loc = "shyroke"
+manager_name = LOCATIONS[loc]["manager"]["name"]
+print(manager_name)  # => Oleksandr Rudnov | Олександр Руднов
+
 
 @restricted
 async def contact_location_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
---
+++ locations.py
@@ -0,0 +1,47 @@
+# locations.py
+
+LOCATIONS = {
+    "shyroke": {
+        "manager": {
+            "name": "Oleksandr Rudnov | Олександр Руднов",
+            "phone": "+380987938674",
+            "email": "OleRud441@npaid.org"
+        },
+        "senior_officer": {
+            "position": "Senior Fleet Officer",
+            "name": "Roman Kucherevskyi",
+            "phone": "+380661930132",
+            "email": "RomKuc884@npaid.org"
+        },
+        "car_washes": [
+            {"name": "Avtoynhulstroy", "phone": "+380 67 633 1025", "map": "https://www.google.com/maps?cid=3778105884522161440"},
+            {"name": "Nova Liniya", "phone": "+380 97 577 2770", "map": "https://www.google.com/maps?cid=1167848751790635382"},
+        ],
+        "tire_services": [
+            {"name": "Avtoynhulstroy", "phone": "+380 67 633 1025", "map": "https://www.google.com/maps?cid=3778105884522161440"},
+            {"name": "Nova Liniya", "phone": "+380 97 577 2770", "map": "https://www.google.com/maps?cid=1167848751790635382"},
+            {"name": "SHYROKE - Tyre service", "phone": "+380 98 455 8113", "map": "https://maps.app.goo.gl/otgcPE4GaHowdxEj8"},
+        ],
+    },
+    "mykolaiv": {
+        "manager": {
+            "name": "Andriy Padalka | Андрій Падалка",
+            "phone": "+380506008345",
+            "email": "AndPad212@npaid.org"
+        },
+        "senior_officer": {
+            "position": "Senior Fleet Officer",
+            "name": "Roman Kucherevskyi",
+            "phone": "+380661930132",
+            "email": "RomKuc884@npaid.org"
+        },
+        "car_washes": [
+            {"name": "AVTOSMILE", "phone": "+380 95 792 35 94", "map": "https://maps.app.goo.gl/Xc2b4q8CWEHERMtL9"},
+            {"name": "VIANOR", "phone": "+380 50 495 60 85", "map": "https://maps.app.goo.gl/HQw66xsakjPp245T6"},
+        ],
+        "tire_services": [
+            {"name": "Tire Service 1", "phone": "+380 432 111 001", "map": "https://goo.gl/maps/tire1_mykolaiv"},
+            {"name": "Tire Service 2", "phone": "+380 432 111 002", "map": "https://goo.gl/maps/tire2_mykolaiv"},
+        ],
+    }
+}
